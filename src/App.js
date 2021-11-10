1b8bcc2c4be9e3a3cb14492def98625968de13f4
Muhammad Talha <talhaofficialwork@gmail.com>
2021-11-10 16:12:42 +0500

    Finalized Design of Loading Animation Screen

--- src/App.js
+++ src/App.js
@@ -1,89 +1,83 @@
-import React, { useEffect } from 'react';
-import useWebAnimations from "@wellyshen/use-web-animations";
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+
+var keyFramesOrange = [
+  { background: "#FFA500", transform: "translateY(10px)" },
+  { background: "white" },
+  { background: "#FFA500", transform: "translateY(0px)" },
+  { background: "white" },
+  { background: "#FFA500", transform: "translateY(-10px)" },
+];
+
+var keyFramesGreen = [
+  { background: "#90EE90", transform: "translateY(-12px)" },
+  { background: "white" },
+  { background: "#90EE90", transform: "translateY(0px)" },
+  { background: "white" },
+  { background: "#90EE90", transform: "translateY(12px)" },
+];
+
+var keyFramesBlue = [
+  { background: "#87CEFA", transform: "translateY(15px)" },
+  { background: "white" },
+  { background: "#87CEFA", transform: "translateY(0px)" },
+  { background: "white" },
+  { background: "#87CEFA", transform: "translateY(-15px)" },
+];
+
+var keyFramesPink = [
+  { background: "#FFB6C1", transform: "translateY(-12px)" },
+  { background: "white" },
+  { background: "#FFB6C1", transform: "translateY(0px)" },
+  { background: "white" },
+  { background: "#FFB6C1", transform: "translateY(12px)" },
+];
+
+var keyFramesYellow = [
+  { background: "#FFFF00", transform: "translateY(10px)" },
+  { background: "white" },
+  { background: "#FFFF00", transform: "translateY(0px)" },
+  { background: "white" },
+  { background: "#FFFF00", transform: "translateY(-10px)" },
+];
+
+var keyProps = {
+  duration: 1000,
+  iterations: Infinity,
+  direction: "alternate",
+};
 
 function App() {
-
-  const main = useWebAnimations({
-    keyframes: [
-      { backgroundPosition: '0% 0%' },
-      { backgroundPosition: '100% 0%' },
-
-    ],
-    timing: {
-      duration: 5000,
-      iterations: Infinity,
-      easing: "linear",
-    }
-  });
-  const clouds = useWebAnimations({
-    keyframes: [
-      { backgroundPosition: '250% 0%' },
-      { backgroundPosition: '-150% 0%' },
-
-    ],
-    timing: {
-      duration: 5000,
-      iterations: Infinity,
-      easing: "linear",
-    }
-  });
-  const running_object = useWebAnimations({
-    keyframes: [
-      { backgroundPosition: '0% 0%' },
-      { backgroundPosition: '0% 0%' },
-
-    ],
-    timing: {
-      duration: 30000,
-      iterations: Infinity,
-      easing: "linear",
-    }
-  });
-
-  function fastpace() {
-    running_object.animate({
-      keyframes: [
-        { backgroundPosition: '0% 0%' },
-        { backgroundPosition: '20% 0%' },
-        { backgroundPosition: '40% 0%' },
-        { backgroundPosition: '60% 0%' },
-        { backgroundPosition: '80% 0%' },
-
-      ],
-      timing: { duration: 30500, fill: "forwards" },
-
-    });
-    clouds.getAnimation().updatePlaybackRate(clouds.getAnimation().playbackRate * 1.15);
-    main.getAnimation().updatePlaybackRate(main.getAnimation().playbackRate * 1.15);
-  }
-
-  const reset = () => {
-    clouds.getAnimation().playbackRate = 1;
-    main.getAnimation().playbackRate = 1;
-  };
-
   useEffect(() => {
-    setInterval(() => {
-      if (main.getAnimation().playbackRate > 3) {
-        clouds.getAnimation().updatePlaybackRate(clouds.getAnimation().playbackRate * 0.8);
-        main.getAnimation().updatePlaybackRate(main.getAnimation().playbackRate * 0.8);
-      }
-      else if (main.getAnimation().playbackRate > 2) {
-        clouds.getAnimation().updatePlaybackRate(clouds.getAnimation().playbackRate * 1.8);
-        main.getAnimation().updatePlaybackRate(main.getAnimation().playbackRate * 1.8);
-      }
-    }
-      , 1500)
 
-  }, [clouds, main, running_object])
+    var shapeOrange = document.querySelector(".orange");
+    var shapeGreen = document.querySelector(".green");
+    var shapeBlue = document.querySelector(".blue");
+    var shapePink = document.querySelector(".pink");
+    var shapeYellow = document.querySelector(".yellow");
+
+    function animation() {
+      shapeOrange.animate(keyFramesOrange, keyProps);
+      shapeGreen.animate(keyFramesGreen, keyProps);
+      shapeBlue.animate(keyFramesBlue, keyProps);
+      shapePink.animate(keyFramesPink, keyProps);
+      shapeYellow.animate(keyFramesYellow, keyProps);
+    }
+
+    return animation();
+
+  }, []);
 
   return (
-    <div className="mainbody" ref={main.ref} >
-      <div className="clouds" ref={clouds.ref}></div>
-      <div className="cowboy" onClick={reset} ref={running_object.ref} ></div>
-      <div className="lion" onClick={fastpace} ref={running_object.ref} ></div>
-    </div>
+    <>
+      <div class="container">
+        <div class="center orange"></div>
+        <div class="center green"></div>
+        <div class="center blue"></div>
+        <div class="center pink"></div>
+        <div class="center yellow"></div>
+      </div>
+    </>
   );
 }
 
